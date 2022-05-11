7110f16fb0ee970a87e4e0cdf70a72db2f5757df
Jewoo <jeus0630@gmail.com>
2022-05-12 00:52:21 +0900

    폴더 추가 / story 네이밍 변경

--- src/components/input/input.stories.tsx
+++ src/components/input/input.stories.tsx
@@ -1,10 +1,12 @@
 import InputComponent from "./Input";
 
 export default {
-    title: 'Input',
+    title: 'form/Input',
     component: InputComponent
 }
 
 export const Small = () => <InputComponent size="small"></InputComponent>
 export const Medium = () => <InputComponent size="medium"></InputComponent>
 export const Large = () => <InputComponent size="large"></InputComponent>
+
+Small.storyName = 'Small!'
