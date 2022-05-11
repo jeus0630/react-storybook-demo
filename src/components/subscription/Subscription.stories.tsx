3a1013550ed721684c036ac24a90c4f26db51499
Jewoo <jeus0630@gmail.com>
2022-05-12 01:08:21 +0900

    Write Story within Story

---
+++ src/components/subscription/Subscription.stories.tsx
@@ -0,0 +1,13 @@
+import { Primary } from "../button/Button.stories";
+import { Large } from "../input/Input.stories";
+
+export default {
+    title: 'form/Subscription'
+}
+
+export const PrimarySubscription = () => (
+    <>
+        <Large></Large>
+        <Primary></Primary>
+    </>
+)
